6806d815b0b69982cf9e714956877a38a2e76577
ofarukcelik <ofarukcelik@outlook.com>
2021-02-19 20:10:37 +0300

    simulated

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+using System.Linq;
 
 namespace ConsoleUI
 {
@@ -6,7 +11,16 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BrandManager brandManager = new BrandManager(new InMemoryBrandDal());
+            CarColorManager carColorManager = new CarColorManager(new InMemoryCarColorDal());
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+
+            foreach (Car car in carManager.GetAll())
+            {
+                string brand = brandManager.GetAll().Single(b => b.BrandID == car.BrandID).BrandName;
+                string carColor = carColorManager.GetAll().Single(cc => cc.ColorID == car.CarColorID).ColorName;
+                Console.WriteLine(car.ID + " " + brand + " " + carColor + " " + car.ModelYear + " " + car.DailyPrice + " " + car.Description);
+            }
         }
     }
 }
