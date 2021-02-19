4a913f028d087996b487613939896a94b1ccaa68
ofarukcelik <ofarukcelik@outlook.com>
2021-02-19 19:32:42 +0300

    Manage classes created and implemented

---
+++ Business/Concrete/CarColorManager.cs
@@ -0,0 +1,24 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CarColorManager : ICarColorService
+    {
+        ICarColorDal _carColorDal;
+
+        public CarColorManager(ICarColorDal carColorDal)
+        {
+            _carColorDal = carColorDal;
+        }
+
+        public List<CarColor> GetAll()
+        {
+            return _carColorDal.GetAll();
+        }
+    }
+}
