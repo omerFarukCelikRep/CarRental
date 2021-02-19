74be456be90046c1dced1b55620b7202d06e0eaf
ofarukcelik <ofarukcelik@outlook.com>
2021-02-20 01:24:12 +0300

    Linq join method used

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -15,11 +15,24 @@ namespace ConsoleUI
             CarColorManager carColorManager = new CarColorManager(new InMemoryCarColorDal());
             CarManager carManager = new CarManager(new InMemoryCarDal());
 
-            foreach (Car car in carManager.GetAll())
+            var carList = carManager.GetAll().Join(brandManager.GetAll(),
+                cm => cm.BrandID,
+                bm => bm.BrandID,
+                (cm, bm) => new { cm.ID, bm.BrandName, cm.CarColorID, cm.ModelYear, cm.DailyPrice, cm.Description }).Join(carColorManager.GetAll(),
+                cl => cl.CarColorID,
+                ccm => ccm.ColorID,
+                (cl, ccm) => new { cl.ID, cl.BrandName, ccm.ColorName, cl.ModelYear, cl.DailyPrice, cl.Description });
+
+            //foreach (Car car in carManager.GetAll())
+            //{
+            //    string brand = brandManager.GetAll().Single(b => b.BrandID == car.BrandID).BrandName;
+            //    string carColor = carColorManager.GetAll().Single(cc => cc.ColorID == car.CarColorID).ColorName;
+            //    Console.WriteLine(car.ID + " " + brand + " " + carColor + " " + car.ModelYear + " " + car.DailyPrice + " " + car.Description);
+            //}
+
+            foreach (var car in carList)
             {
-                string brand = brandManager.GetAll().Single(b => b.BrandID == car.BrandID).BrandName;
-                string carColor = carColorManager.GetAll().Single(cc => cc.ColorID == car.CarColorID).ColorName;
-                Console.WriteLine(car.ID + " " + brand + " " + carColor + " " + car.ModelYear + " " + car.DailyPrice + " " + car.Description);
+                Console.WriteLine($"{car.ID} {car.BrandName} {car.ColorName} {car.ModelYear} {car.DailyPrice} {car.Description}");
             }
         }
     }
