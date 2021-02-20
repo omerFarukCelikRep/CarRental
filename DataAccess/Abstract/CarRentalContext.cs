bb2f8b0c3b922c638212bc4765c56ab58ecbb105
ofarukcelik <ofarukcelik@outlook.com>
2021-02-21 00:20:51 +0300

    DbContext created

---
+++ DataAccess/Abstract/CarRentalContext.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    public class CarRentalContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-3EVB481;Database=Northwind;Trusted_Connection=true");
+        }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<CarColor> CarColors { get; set; }
+    }
+}
