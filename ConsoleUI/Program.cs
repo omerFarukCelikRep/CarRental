f5c36a9ec9d7f5c80c4d70f2b3d212bc8120b009
ofarukcelik <ofarukcelik@outlook.com>
2021-02-22 19:55:24 +0300

    CarDetail test

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -12,45 +12,9 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            BrandManager brandManager = new BrandManager(new EfBrandDal());
-            CarColorManager carColorManager = new CarColorManager(new EfCarColorDal());
             CarManager carManager = new CarManager(new EfCarDal());
 
-            EfCarDal efCarDal = new EfCarDal();
-            efCarDal.Add(new Car
-            {
-                BrandID = 3,
-                CarColorID = 1,
-                ModelYear = 2021,
-                DailyPrice = 80,
-                Description = "Çok Yakar"
-            });
-
-            var carList = carManager.GetAll().Join(brandManager.GetAll(),
-                cm => cm.BrandID,
-                bm => bm.BrandID,
-                (cm, bm) => new
-                {
-                    cm.CarID,
-                    bm.BrandName,
-                    cm.CarColorID,
-                    cm.ModelYear,
-                    cm.DailyPrice,
-                    cm.Description
-                }).Join(carColorManager.GetAll(),
-                cl => cl.CarColorID,
-                cc => cc.CarColorID,
-                (cl, cc) => new
-                {
-                    cl.CarID,
-                    cl.BrandName,
-                    cc.CarColorName,
-                    cl.ModelYear,
-                    cl.DailyPrice,
-                    cl.Description
-                }).ToList();
-
-            foreach (var car in carList)
+            foreach (var car in carManager.GetCarDetails())
             {
                 Console.WriteLine($"{car.CarID} {car.BrandName} {car.CarColorName} {car.ModelYear} {car.DailyPrice} {car.Description}");
             }
