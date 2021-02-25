37ecc8f7a6986e52092aa068dba20dcb8f82794d
ofarukcelik <ofarukcelik@outlook.com>
2021-02-26 00:55:23 +0300

    Messages created

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string CarAdded = "Car Added";
+
+        public static string BrandAdded = "Brand Added";
+
+        public static string BrandDeleted = "Brand Deleted";
+
+        public static string BrandsListed = "Brands Listed";
+
+        public static string BrandUpdated = "Brand Updated";
+
+        public static string CarColorAdded = "Car Color Added";
+
+        public static string CarColorDeleted = "Car Color Deleted";
+
+        public static string CarColorsListed = "Car Colors Listed";
+
+        public static string CarColorUpdated = "Car Color Updated";
+
+        public static string CarsListed = "Cars Listed";
+
+        public static string CarDeleted = "Car Deleted";
+
+        public static string CarsListedByBrandID = "Cars Listed By BrandID";
+
+        public static string CarsListedByCarColorID = "Cars Listed By CarColorID";
+
+        public static string CarDetailsListed = "Car Details Listed";
+
+        public static string CarsListedByDailyPrice = "Cars Listed By Daily Price";
+
+        public static string CarUpdated = "Car Updated";
+    }
+}
