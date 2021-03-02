cfc7b754949cd84062de18d06b53f0cc7d8c147e
ofarukcelik <ofarukcelik@outlook.com>
2021-03-02 17:05:32 +0300

    Customer, Rental and User Entities added with properties

---
+++ Entities/Concrete/Rental.cs
@@ -0,0 +1,19 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Rental : IEntity
+    {
+        public int RentalID { get; set; }
+        public DateTime RentDate { get; set; }
+        public DateTime? ReturnDate { get; set; }
+
+        public int CarID { get; set; }
+        public virtual Car Car { get; set; }
+        public int CustomerID { get; set; }
+        public virtual Customer Customer { get; set; }
+    }
+}
