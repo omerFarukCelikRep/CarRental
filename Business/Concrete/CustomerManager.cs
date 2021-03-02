665eccb50f11df491d98fed4e5768a448cbf6175
ofarukcelik <ofarukcelik@outlook.com>
2021-03-02 19:39:58 +0300

    New Business classes added

---
+++ Business/Concrete/CustomerManager.cs
@@ -0,0 +1,45 @@
+﻿using Business.Abstract;
+using Business.Constants;
+using Core.Utilities.Results.Abstract;
+using Core.Utilities.Results.Concrete;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CustomerManager : ICustomerService
+    {
+        ICustomerDal _customerDal;
+
+        public CustomerManager(ICustomerDal customerDal)
+        {
+            _customerDal = customerDal;
+        }
+
+        public IResult Add(Customer entity)
+        {
+            _customerDal.Add(entity);
+            return new SuccessResult(Messages.CustomerAdded);
+        }
+
+        public IResult Delete(Customer entity)
+        {
+            _customerDal.Delete(entity);
+            return new SuccessResult(Messages.CustomerDeleted);
+        }
+
+        public IDataResult<List<Customer>> GetAll()
+        {
+            return new SuccessDataResult<List<Customer>>(_customerDal.GetAll(), Messages.CustomersListed);
+        }
+
+        public IResult Update(Customer entity)
+        {
+            _customerDal.Update(entity);
+            return new SuccessResult(Messages.CustomerUpdated);
+        }
+    }
+}
---
+++ Business/Concrete/RentalManager.cs
@@ -0,0 +1,45 @@
+﻿using Business.Abstract;
+using Business.Constants;
+using Core.Utilities.Results.Abstract;
+using Core.Utilities.Results.Concrete;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class RentalManager : IRentalService
+    {
+        IRentalDal _rentalDal;
+
+        public RentalManager(IRentalDal rentalDal)
+        {
+            _rentalDal = rentalDal;
+        }
+
+        public IResult Add(Rental entity)
+        {
+            _rentalDal.Add(entity);
+            return new SuccessResult(Messages.RentalAdded);
+        }
+
+        public IResult Delete(Rental entity)
+        {
+            _rentalDal.Delete(entity);
+            return new SuccessResult(Messages.RentalDeleted);
+        }
+
+        public IDataResult<List<Rental>> GetAll()
+        {
+            return new SuccessDataResult<List<Rental>>(_rentalDal.GetAll(), Messages.RentalsListed);
+        }
+
+        public IResult Update(Rental entity)
+        {
+            _rentalDal.Update(entity);
+            return new SuccessResult(Messages.RentalUpdated);
+        }
+    }
+}
